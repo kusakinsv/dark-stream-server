f0b0cf6b86c53652555bb5ae1e6ad2bfb8f087e9
Sergei <84863854+kusakinsv@users.noreply.github.com>
2022-06-13 19:01:01 +0500

    Стабильная версия 0.0.2а Добавлен поиск по базе

--- src/main/java/ru/dark/stream/entities/PlaylistMusicTrack.java
+++ src/main/java/ru/dark/stream/entities/PlaylistMusicTrack.java
@@ -2,13 +2,13 @@ package ru.dark.stream.entities;
 
 import lombok.Data;
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.List;
-@Data
 @Entity
 @Getter
-
+@Setter
 @Table(name = "playlist_music_track")
 public class PlaylistMusicTrack {
 
@@ -25,4 +25,21 @@ public class PlaylistMusicTrack {
         @OneToOne(cascade = CascadeType.ALL)
         @JoinColumn(name = "music_track", referencedColumnName = "id")
         MusicTrack musicTrack;
-    }
+
+        @Override
+        public boolean equals(Object o) {
+                if (this == o) return true;
+                if (!(o instanceof PlaylistMusicTrack)) return false;
+
+                PlaylistMusicTrack that = (PlaylistMusicTrack) o;
+
+                return getMusicTrack().equals(that.getMusicTrack());
+        }
+
+        @Override
+        public int hashCode() {
+                return getId() != null ? getId().hashCode() : 0;
+        }
+}
+
+
--- src/main/java/ru/dark/stream/service/PlaylistService.java
+++ src/main/java/ru/dark/stream/service/PlaylistService.java
@@ -41,9 +41,8 @@ public class PlaylistService {
     }
 
     public void addToPlayList(MusicTrack musicTrack) {
-
         PlaylistMusicTrack playlistMusicTrack = new PlaylistMusicTrack();
-        PlaylistMusicTrack findedplaylist = new PlaylistMusicTrack();
+        PlaylistMusicTrack findedplaylist;
         MusicTrack trackForFound = null;
         trackForFound = musicTrackService.find(musicTrack);
         if (trackForFound == null) {
@@ -57,15 +56,46 @@ public class PlaylistService {
         }
         try {
             findedplaylist = find(playlistMusicTrack);
+            System.out.println(1111111);
         } catch (Exception e) {
             findedplaylist = playlistMusicTrack;
+            System.out.println(2222222);
         }
-        session.beginTransaction();
-        session.save(findedplaylist);
-        session.getTransaction().commit();
-
-
+        if(findedplaylist == null){
+            findedplaylist = playlistMusicTrack;
+            session.beginTransaction();
+            session.save(findedplaylist);
+            session.getTransaction().commit();
+        }
     }
+
+//    public void addToPlayList(MusicTrack musicTrack) {
+//
+//        PlaylistMusicTrack playlistMusicTrack = new PlaylistMusicTrack();
+//        PlaylistMusicTrack findedplaylist;
+//        MusicTrack trackForFound = null;
+//        trackForFound = musicTrackService.find(musicTrack);
+//        if (trackForFound == null) {
+//            playlistMusicTrack.setMusicTrack(musicTrack);
+//            playlistMusicTrack.setNumber(getCountOfCompositions() + 1);
+//            System.out.println("CREATED: " + playlistMusicTrack.getMusicTrack().getTrackInfo() + " " + playlistMusicTrack.getNumber());
+//        } else {
+//            playlistMusicTrack.setMusicTrack(trackForFound);
+//            playlistMusicTrack.setNumber(getCountOfCompositions() + 1);
+//            System.out.println("ДАННЫЙ ТРЕК УЖЕ ЕСТЬ: " + playlistMusicTrack.getMusicTrack().getTrackInfo() + " " + playlistMusicTrack.getNumber());
+//        }
+//        try {
+//            findedplaylist = find(playlistMusicTrack);
+//            System.out.println(1111111);
+//        } catch (Exception e) {
+//            findedplaylist = playlistMusicTrack;
+//            System.out.println(2222222);
+//        }
+//        System.out.println(findedplaylist.toString());
+//        session.beginTransaction();
+//        session.save(findedplaylist);
+//        session.getTransaction().commit();
+//    }
 
     public PlaylistMusicTrack find(PlaylistMusicTrack playlistMusicTrack) {
         Criteria criteria = session.createCriteria(PlaylistMusicTrack.class);
